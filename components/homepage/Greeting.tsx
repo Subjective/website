5d8ecfd4c77a5c925d365cd6098226f9ae6f7cae
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-30 00:16:47 -0700

    add greeting with colored gradient

---
+++ components/homepage/Greeting.tsx
@@ -0,0 +1,10 @@
+export default function Greeting() {
+  return (
+    <>
+      <h1 className="text-8xl font-extrabold tracking-tight leading-tight text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        Hey there, stranger!
+      </h1>
+    </>
+  )
+  return
+}
