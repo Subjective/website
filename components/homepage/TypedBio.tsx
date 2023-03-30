6788457bed26e3f7265b8550ebbbe910a3fc2093
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-30 00:16:35 -0700

    add typed bio

---
+++ components/homepage/TypedBio.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Typed from 'typed.js'
+
+export default function TypedBios() {
+  let el = React.useRef(null)
+  let typed = React.useRef(null)
+
+  React.useEffect(() => {
+    typed.current = new Typed(el.current, {
+      stringsElement: '#bios',
+      typeSpeed: 40,
+      backSpeed: 10,
+      loop: true,
+      backDelay: 1000,
+    })
+    return () => typed.current.destroy()
+  }, [])
+
+  return (
+    <div className="text-4xl">
+      <ul id="bios" className="hidden">
+        <li>I'm Josh.</li>
+        <li>I'm a programmer.</li>
+        <li>I'm a programmer.</li>
+        <li>I'm a lifelong learner.</li>
+        <li>I'm a problem solver.</li>
+        <li>I'm an aspiring entrepreneur</li>
+        <li>I'm a night owl.</li>
+        <li>I'm a</li>
+      </ul>
+      <span ref={el} className="text-neutral-900 dark:text-neutral-200" />
+    </div>
+  )
+}
