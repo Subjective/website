f70ddecd618141bed9e998c47043f82af5fd1882
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-31 11:33:47 -0700

    chore: format tailwind classes

--- components/homepage/Greeting.tsx
+++ components/homepage/Greeting.tsx
@@ -1,12 +1,9 @@
 export default function Greeting() {
   return (
     <>
-      {/* <h1 className="text-8xl font-extrabold tracking-tight leading-tight text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 sm:text-4xl md:text-8xl"> */}
-      <h1 className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-5xl font-extrabold leading-tight tracking-tight text-transparent sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
-        {/* <h1 className="text-8xl font-extrabold tracking-tight leading-tight text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"> */}
+      <h1 className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-3xl font-extrabold leading-tight tracking-tight text-transparent sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
         Hey there, stranger!
       </h1>
     </>
   )
-  return
 }
