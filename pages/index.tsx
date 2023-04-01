7e3cc759fd099beb880f6fa89431c954732fdee8
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-01 15:29:01 -0700

    fix: change link component to button

--- pages/index.tsx
+++ pages/index.tsx
@@ -137,14 +137,13 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
           <TypedBio />
         </div>
       </div>
-      <Link
+      <button
         style={{
           top: '90%',
           left: '50%',
           transform: 'translateX(-50%)',
         }}
         aria-hidden={'true'}
-        role={'button'}
         className={'focus-invisible absolute animate-bounce duration-500 hover:opacity-50'}
         onClick={(e) => {
           e.preventDefault()
@@ -170,7 +169,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
             d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
           />
         </svg>
-      </Link>
+      </button>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1
