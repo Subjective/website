92f6ac075d1c2ca274afd6e567c53dcf327096c6
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-01 15:20:40 -0700

    fix: decrease greeting width

--- components/homepage/Greeting.tsx
+++ components/homepage/Greeting.tsx
@@ -1,7 +1,7 @@
 export default function Greeting() {
   return (
     <>
-      <h1 className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-6xl font-extrabold leading-tight tracking-tight text-transparent sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
+      <h1 className="w-4/5 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-6xl font-extrabold leading-tight tracking-tight text-transparent sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
         Hey there, stranger!
       </h1>
     </>
