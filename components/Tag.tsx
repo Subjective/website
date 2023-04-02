2766ba1d840fbefce2248c24a66e87a2106d5959
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-02 01:34:13 -0700

    fix: blog div click handler takes precedence over tag link

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { kebabCase } from 'pliny/utils/kebabCase'
 
 interface Props {
@@ -6,10 +7,21 @@ interface Props {
 }
 
 const Tag = ({ text }: Props) => {
+  const router = useRouter()
+
   return (
     <Link
       href={`/tags/${kebabCase(text)}`}
       className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      onClick={(e) => {
+        e.stopPropagation()
+      }}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          router.push(`/tags/${kebabCase(text)}`)
+        }
+      }}
+      role="link"
     >
       {text.split(' ').join('-')}
     </Link>
