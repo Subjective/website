f283e69441744b132acb7d59dbf5b92d5b401293
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-02 16:25:43 -0700

    fix: navigate down arrow not displaying in dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -220,7 +220,26 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
             })
           }}
         >
-          <Image src="/static/images/down-arrow.webp" width={35} height={35} alt="down arrow" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="auto"
+            viewBox="0 0 256 256"
+            fill="currentColor"
+          >
+            <g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+              <path
+                d="M 90 24.25 c 0 -0.896 -0.342 -1.792 -1.025 -2.475 c -1.366 -1.367 -3.583 -1.367 -4.949 0 L 45 60.8 L 5.975 21.775 c -1.367 -1.367 -3.583 -1.367 -4.95 0 c -1.366 1.367 -1.366 3.583 0 4.95 l 41.5 41.5 c 1.366 1.367 3.583 1.367 4.949 0 l 41.5 -41.5 C 89.658 26.042 90 25.146 90 24.25 z"
+                style={{
+                  stroke: 'none',
+                  strokeWidth: 1,
+                  strokeDasharray: 'none',
+                  strokeLinecap: 'butt',
+                  strokeLinejoin: 'miter',
+                }}
+              />
+            </g>
+          </svg>
         </button>
       </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
