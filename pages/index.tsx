46e84a481c7b89cdea411a81c286b794d2ca2703
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-02 12:59:19 -0700

    feat: use minimal scroll down arrow

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import { sortedBlogPost, allCoreContent } from 'pliny/utils/contentlayer'
 import { InferGetStaticPropsType } from 'next'
+import Image from 'next/image'
 import { allBlogs } from 'contentlayer/generated'
 import type { Blog } from 'contentlayer/generated'
 import Greeting from '@/components/homepage/Greeting'
@@ -155,19 +156,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
             })
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="35px"
-            height="auto"
-            fill="currentColor"
-            className="bi bi-arrow-down-circle"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule={'evenodd'}
-              d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
-            />
-          </svg>
+          <Image src="/static/images/down-arrow.webp" width={35} height={35} alt="down arrow" />
         </button>
       </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
