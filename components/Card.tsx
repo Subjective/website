b2284d17b6e1fcf28cc655d2b58c607c5cbd9d35
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-05 17:57:06 -0700

    feat: make projects card clickable

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,12 @@
 import Image from './Image'
 import Link from './Link'
+import ClickableCard from './ClickableCard'
 import ScrollAnimationWrapper from './ScrollAnimationWrapper'
 
 const Card = ({ title, description, imgSrc, href }) => (
   <ScrollAnimationWrapper animateInitialUp={true} className="md max-w-[544px] p-4 md:w-1/2">
-    <div
+    <ClickableCard
+      link={href}
       className={`${
         imgSrc && 'h-full'
       }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 hover:border-primary-400 dark:border-gray-700 dark:hover:border-primary-500`}
@@ -18,6 +20,7 @@ const Card = ({ title, description, imgSrc, href }) => (
               className="object-cover object-center md:h-36 lg:h-48"
               width={544}
               height={306}
+              onClick={(e) => e.stopPropagation()}
             />
           </Link>
         ) : (
@@ -32,7 +35,7 @@ const Card = ({ title, description, imgSrc, href }) => (
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
+            <Link href={href} aria-label={`Link to ${title}`} onClick={(e) => e.stopPropagation()}>
               {title}
             </Link>
           ) : (
@@ -45,12 +48,13 @@ const Card = ({ title, description, imgSrc, href }) => (
             href={href}
             className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label={`Link to ${title}`}
+            onClick={(e) => e.stopPropagation()}
           >
             Learn more &rarr;
           </Link>
         )}
       </div>
-    </div>
+    </ClickableCard>
   </ScrollAnimationWrapper>
 )
 
