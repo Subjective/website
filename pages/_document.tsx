bec823828af5263608a25cc356732c231d622c43
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-09 21:34:09 -0700

    feat: support dark mode for favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,17 +11,34 @@ class MyDocument extends Document {
             sizes="180x180"
             href="/static/favicons/apple-touch-icon.png"
           />
+          <link rel="icon" type="image/svg+xml" href="/static/favicons/favicon.svg" />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
             href="/static/favicons/favicon-32x32.png"
+            media="(prefers-color-scheme: light)"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-dark-32x32.png"
+            media="(prefers-color-scheme: dark)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-dark-16x16.png"
+            media="(prefers-color-scheme: dark)"
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
